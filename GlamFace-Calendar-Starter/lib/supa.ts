fa3551f7132afd90843e847a807e631d145f5fb9
Loredana-diciaula <loredana.diciaula@gmail.com>
2025-09-21 17:56:38 +0200

    Create supa.ts

---
+++ GlamFace-Calendar-Starter/lib/supa.ts
@@ -0,0 +1,7 @@
+// lib/supa.ts
+import { createClient } from '@supabase/supabase-js'
+
+export const supa = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
